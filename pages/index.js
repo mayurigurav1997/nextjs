9835baa6a09213cb555cd1057bcca19c639ca414
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-28 19:25:11 +0530

    Blog Added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-export default function Home() {
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+function Home() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('Placing your order')
+    router.push('/product')
+  }
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>Welcome Home</h1>
+      <Link href='/blog'>
+        Blog
+      </Link>
+      <Link href='/product'>
+        Products
+      </Link>
+      <button onClick={handleClick}>Place Order</button>
     </>
   )
 }
+
+export default Home
