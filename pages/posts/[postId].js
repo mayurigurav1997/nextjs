ea41eef27f6d01e972bebcdc555be265f1cbc07a
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-30 11:31:12 +0530

    fallback false added

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -40,21 +40,21 @@ export async function getStaticProps(context) {
     }
 }
 
-// export async function getStaticPaths() {
-//     // const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-//     // const data = await response.json()
-//     // const paths = data.map(post => {
-//     //   return {
-//     //     params: { postId: `${post.id}` }
-//     //   }
-//     // })
+export async function getStaticPaths() {
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    // const data = await response.json()
+    // const paths = data.map(post => {
+    //   return {
+    //     params: { postId: `${post.id}` }
+    //   }
+    // })
 
-//     return {
-//         paths: [
-//             { params: { postId: '1' } },
-//             { params: { postId: '2' } },
-//             { params: { postId: '3' } }
-//         ],
-//         fallback: true
-//     }
-// }
+    return {
+        paths: [
+            { params: { postId: '1' } },
+            { params: { postId: '2' } },
+            { params: { postId: '3' } }
+        ],
+        fallback: false
+    }
+}
