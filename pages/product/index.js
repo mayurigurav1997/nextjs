ed7bcb79742f739eb876d6f2639bebc0eb5036cb
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-08-31 16:54:46 +0530

    product details

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
-const product = () => {
+const product = ({ productId = 100 }) => {
     return (
         <>
-            <h1>Product 1</h1>
-            <h1>Product 2</h1>
-            <h1>Product 3</h1>
+            <p><Link href="/">HOME</Link></p>
+            <h1><Link href="/product/1">Product 1</Link></h1>
+            <h1><Link href="/product/2">Product 2</Link></h1>
+            <h1><Link href="/product/3" replace>Product 3</Link></h1>
+            <h1><Link href={`/product/${productId}`} replace>Product 100</Link></h1>
         </>
     )
 }
