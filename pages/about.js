fadd5a00a59f51d5c5c3e6216e36dcd15f3a41ce
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-09-02 15:45:24 +0530

    component level styling

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 const about = () => {
     return (
         <>
             <h2>About Page</h2>
             <button className="btn btn-primary">Primary</button>
+            <h2 className={styles.highlight}>Styles Highlight</h2>
         </>
     )
 }
